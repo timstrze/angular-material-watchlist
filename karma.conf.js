81922ee5b6186698a97c3519fe867dba7b87e4b6
Timothy Strzelecki <tstrzelecki@kaplan.edu>
2017-09-08 17:57:06 -0400

    Updated karma and package.json to latest

--- karma.conf.js
+++ karma.conf.js
@@ -12,18 +12,12 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
     files: [
       { pattern: './src/test.ts', watched: false },
       { pattern: './node_modules/@angular/material/prebuilt-themes/indigo-pink.css' }
     ],
-    preprocessors: {
-      './src/test.ts': ['@angular/cli']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       reports: [ 'html', 'lcovonly' ],
@@ -32,14 +26,12 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'], //Chrome
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
