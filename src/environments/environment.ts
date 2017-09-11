0d2e8955f462072421ed0b92644020eb90d6bd37
Timothy Strzelecki <timstrze@gmail.com>
2017-09-11 12:44:13 -0400

    Adding firebase db and authentication to app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBPdnUpeA0H8hG9Nes2KXDFJjmzLXtSW5k',
+    authDomain: 'watchlist-309b0.firebaseapp.com',
+    databaseURL: 'https://watchlist-309b0.firebaseio.com',
+    projectId: 'watchlist-309b0',
+    storageBucket: 'watchlist-309b0.appspot.com',
+    messagingSenderId: '522654964268'
+  }
 };
