b44075391edad257ca2601a5f488ac0b50e46753
Timothy Strzelecki <timstrze@gmail.com>
2017-09-11 12:44:13 -0400

    Adding firebase db and authentication to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from './user/user.service';
 import {IUser} from './user/user.interface';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,18 @@ import {IUser} from './user/user.interface';
 export class AppComponent implements OnInit {
 
   User: IUser;
-  constructor(private userService: UserService) {}
+  users: FirebaseListObservable<any[]>;
+  // db: AngularFireDatabase;
+
+  constructor(private userService: UserService, db: AngularFireDatabase) {
+    this.users = db.list('/users');
+  }
 
   ngOnInit() {
-    this.userService.getUser().then((results) => {
-      this.User = results;
-    });
+    // this.userService.getUser().then((results) => {
+    //   console.log(results);
+    //   this.User = results;
+    // });
   }
 
 }
