a70a47f56267788213afec4c492d85a2eb8b0194
Timothy Strzelecki <timstrze@gmail.com>
2017-09-14 10:49:23 -0400

    Initial firebase integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,40 @@
 import {Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
 import {UserService} from './user/user.service';
 import {IUser} from './user/user.interface';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  User: IUser;
-  users: FirebaseListObservable<any[]>;
-  // db: AngularFireDatabase;
+  user: Observable<firebase.User>;
+  db: AngularFireDatabase;
 
-  constructor(private userService: UserService, db: AngularFireDatabase) {
-    this.users = db.list('/users');
+  constructor(public afAuth: AngularFireAuth, db: AngularFireDatabase) {
+    this.user = afAuth.authState;
+    this.db = db;
   }
 
-  ngOnInit() {
-    // this.userService.getUser().then((results) => {
-    //   console.log(results);
-    //   this.User = results;
-    // });
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then((results) => {
+      console.log(results);
+
+      this.db.list('users', { query: { orderByChild: 'email', equalTo: 'timstrze@gmail.com' }}).subscribe((users) => {
+        console.log(users);
+      });
+    });
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
